3221a46e63918d1361037715b02bad3381ed4b10
Leo Smith <p4piiii@proton.me>
2023-10-30 13:11:36 +0100

    Added comments to explain the HTML code

--- html_source/mitre.py
+++ html_source/mitre.py
@@ -1,16 +1,16 @@
 html_panel_mitre = """
 <div>
-    <h1 style="color:#bd93f9">%s</h1><br />
-    <img src="%s" width="100" />
+    <h1 style="color:#bd93f9">%s</h1><br /><!-- title of the mitre attack -->
+    <img src="%s" width="100" /> <!-- base 64 of the image representing the categorie -->
     <table>
         <tr>
             <th>MITRE ATT&CK ID: </th>
-            <th style="color: #f96769">%s</th>
+            <th style="color: #f96769">%s</th> <!-- the attack id -->
         </tr>
     </table>
     <h3 style="color:#71e0cb">Description:</h3>
-    <p>%s</p>
-    %s
-    %s
+    <p>%s</p><!-- the descirpiton of the attack -->
+    %s <!-- the sub techniques -->
+    %s <!-- The  mitigations of the attack -->
 </div>
 """
--- html_source/vulnerabilities.py
+++ html_source/vulnerabilities.py
@@ -1,21 +1,21 @@
 html_panel_vulns = """
 <div>
-    <h1 style="color:#bd93f9">%s</h1><br />
-    <img src="%s" width="100" />
+    <h1 style="color:#bd93f9">%s</h1><br /> <!-- the name of the vuln -->
+    <img src="%s" width="100" /> <!-- the base 64 image of the vulnerability -->
     <table>
         <tr>
             <th>MITRE ATT&CK ID: </th>
-            <th style="color: #f96769">%s</th>
+            <th style="color: #f96769">%s</th> <!-- the mitre attack id -->
         </tr>
     </table>
     <h3 style="color:#71e0cb">Description:</h3>
-    <p>%s</p>
+    <p>%s</p> <!-- the description of the vulnerability -->
     <h3 style="color:#71e0cb">Sample command:</h3>
     <div style="background-color:#3b3e50; padding: 5px 5px 5px 5px;">
-        <p>%s</p>
+        <p>%s</p> <!-- the sample command -->
     </div>
     <h3 style="color:#71e0cb">Resources:</h3>
-    <ul>
+    <ul> <!-- the different external resources -->
         %s
     </ul>
 </div>
